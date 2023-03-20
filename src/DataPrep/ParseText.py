d50c5e34540b6ba251b042a5eaf0e9af3417fd69
CSchank <chris@schankula.ca>
2023-03-19 21:33:08 -0400

    print dict of dicts

--- src/DataPrep/ParseText.py
+++ src/DataPrep/ParseText.py
@@ -17,17 +17,17 @@ def nltk_tree_to_graph(nltk_tree):
     global i, label
     nx_graph = nx.DiGraph()
     parent = i
-    mapping[parent] = node.label()
     for node in nltk_tree:
         if isinstance(node, Tree):
             print("Adding node ", i, " : ", node.label())
+            mapping[parent] = node.label()
             i = i + 1
             nx_graph.add_edge(parent, i)
             nx_graph = nx.compose(nx_graph, nltk_tree_to_graph(node))
         else:
             print("else", node)
-            mapping[i] = node
             i=i+1
+            mapping[i] = node
     return nx_graph
 
 parser = stanford.StanfordParser(model_path="../../stanford-parser-full-2020-11-17/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")

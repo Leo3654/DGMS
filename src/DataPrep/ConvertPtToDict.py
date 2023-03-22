2e12d5bdd02f3048e93ae3f542efdcdeb2c7ba0e
CSchank <chris@schankula.ca>
2023-03-21 22:09:44 -0400

    script to convert pt to dict and save it

--- src/DataPrep/ConvertPtToDict.py
+++ src/DataPrep/ConvertPtToDict.py
@@ -5,6 +5,13 @@ import sys
 # Define the directory path
 dir_path = sys.argv[1]
 
+# Define the directory path for the dictionary files
+dict_dir_path = os.path.join(dir_path, 'dict_files')
+
+# Create the directory if it doesn't exist
+if not os.path.exists(dict_dir_path):
+    os.makedirs(dict_dir_path)
+
 # Iterate over all the files in the directory
 for file_name in os.listdir(dir_path):
     # Check if the file is a .pt file
@@ -21,5 +28,5 @@ for file_name in os.listdir(dir_path):
         
         # Save the dictionary as _dict.pt file
         dict_file_name = file_name.replace('.pt', '_dict.pt')
-        dict_file_path = os.path.join(dir_path, dict_file_name)
+        dict_file_path = os.path.join(dict_dir_path, dict_file_name)
         torch.save(data_dict, dict_file_path)

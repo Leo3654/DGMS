c0eecde37b9b3702534d68a755ae6b3018ae96ff
CSchank <chris@schankula.ca>
2023-03-25 20:01:52 -0400

    final Python AST module

--- src/DataPrep/PythonAST.py
+++ src/DataPrep/PythonAST.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 
 from Constants import *
 
+import asttokens
+
 labels = {}
 words = []
+last_use_map = {}
 
 log = True
 
@@ -14,8 +17,8 @@ def add_new_node(G,i):
     if log:
         print("Adding node", i)
 
-def add_new_edge(G,i,j):
-    G.add_edge(i,j)
+def add_new_edge(G,i,j, edge_attr = constituency):
+    G.add_edge(i,j,edge_attr = edge_attr)
     if log:
         print("Adding edge", i, j)
 
@@ -27,10 +30,12 @@ def const_to_graph(const, parent):
             i = i + 1
             add_new_edge(G,parent,i)
             labels[i] = word
+            words.append(i)
     else:
         i = i + 1
         add_new_edge(G,parent,i)
         labels[i] = str(const)
+        words.append(i)
     return (G, i)
 
 
@@ -60,14 +65,21 @@ def ast_to_networkx(node, *args):
         add_new_node(G,i)
         labels[i] = "Name"
         # add node with constant
-        add_new_edge(G, i, i + 1)
+        add_new_edge(G, i, i + 1, edge_attr=constituency)
         labels[i+1] = node.id
-        words.append(i)
+
+        try:
+            last_use = last_use_map[node.id]
+            add_new_edge(G, i+1, last_use, last_lexical_use)
+        except:
+            pass
+
+        last_use_map[node.id] = i + 1
         i = i + 1
+        words.append(i)
     elif isinstance(node, ast.Store):
         add_new_node(G,i)
-        labels[i] = "="
-        words.append(i)
+        labels[i] = "Store"
     elif isinstance(node, ast.AST):
         add_new_node(G,i)
         labels[i] = node.__class__.__name__
@@ -95,12 +107,21 @@ def ast_to_networkx(node, *args):
 code = "x = 1\ny = 2\nz = x + y * y"
 code2 = "print(\"Hello world\", 5)"
 code3 = "for i in range(4): print(\"Hello World {i}\")"
-tree = ast.parse(code3)
+code4 = "with open('file_path', 'w') as file:\n    file.write('hello world !')"
+tokens_graph = asttokens.ASTTokens(code4, parse=True)
+tree = tokens_graph.tree
+
+print(tokens_graph)
 
 print(ast.dump(tree))
 
 graph = ast_to_networkx(tree)
 
+for node in ast.walk(tokens_graph.tree):
+    if hasattr(node, 'lineno'):
+        print("first token:", node.first_token)
+        print(tokens_graph.get_text_range(node), node.__class__.__name__, tokens_graph.get_text(node))
+        print("---------")
 
 for layer, nodes in enumerate(nx.topological_generations(graph)):
     # `multipartite_layout` expects the layer as a node attribute, so add the
@@ -109,11 +130,24 @@ for layer, nodes in enumerate(nx.topological_generations(graph)):
         graph.nodes[node]["layer"] = layer
 
 print(labels)
-print(words)
+for word in words: print(word, labels[word])
+
+for i in range(len(words)-1):
+     graph.add_edge(words[i],words[i+1], edge_attr=word_ordering)
 
 pos = nx.multipartite_layout(graph, subset_key="layer", align="horizontal")
+
+for k in pos:
+    pos[k][1] = -pos[k][1]
+
 show_labels = True
 
 nx.draw(graph, pos=pos, with_labels=not show_labels)
+
+edge_labels = {}
+for fr, to, dict in list(graph.edges(data=True)):
+    edge_labels[(fr,to)] = dict['edge_attr']
+
+nx.draw_networkx_edge_labels(graph, pos, edge_labels)
 if show_labels: nx.draw_networkx_labels(graph, pos=pos, labels=labels)
 plt.show()
--- src/DataPrep/PythonToProgramGraph.py
+++ src/DataPrep/PythonToProgramGraph.py
@@ -2,19 +2,23 @@ from python_graphs import program_graph
 from python_graphs import program_graph_graphviz
 import sys
 import ast
+from PythonUtils import get_reduced_program_graph, render
 
-program = "x=1\ny=2\nz=x+y"
+program = "x=1"
 
-graph = program_graph.get_program_graph(program)
+graph = get_reduced_program_graph(program)
 
 tree = ast.parse(program)
 
+print(ast.dump(tree))
+
+print(graph.dump_tree())
 
 for node in ast.walk(tree):
     print(node)
     print(node.__dict__)
     #print("children: " + str([x for x in ast.iter_child_nodes(node)]) + "\\n")
 
-print(len(graph.all_nodes()))
+# print(len(graph.all_nodes()))
 
-program_graph_graphviz.render(graph, "graph.png")
+render(graph, "graph.png")

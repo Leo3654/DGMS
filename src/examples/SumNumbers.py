a046f2cf0d483105ce25bbd72c8a0ee369cfa154
CSchank <chris@schankula.ca>
2023-04-06 17:17:50 -0400

    seq numbers example

--- src/examples/SumNumbers.py
+++ src/examples/SumNumbers.py
@@ -6,3 +6,56 @@ def sum_numbers(n):
     for i in range(1,n+1):
         sum = sum + i
     return sum
+
+# Sum the numbers from 1 to n
+def sum_numbers(n):
+    s = 0
+    for i in range(1,n+1):
+        s = s + i
+    return s
+
+# Sum the numbers from 1 to n
+def f(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum = sum + i
+    return sum
+
+# Sum the numbers from 1 to n
+def f(n):
+    product = 0
+    for i in range(1, n+1):
+        product = product + i
+    return product
+
+# Sum the numbers from 1 to n
+def f(n):
+    return sum(i for i in range(1,n+1))
+
+# Multiply the numbers from 1 to n
+def sum_numbers(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum * i
+    return sum
+
+# Sum the numbers from 5 to n
+def sum_numbers(n):
+    sum = 0
+    for i in range(5, n+1):
+        sum = sum + i
+    return sum
+
+# Sum the numbers from 9 to n
+def sum_numbers(n):
+    sum = 0
+    for i in range(9, n+1):
+        sum = sum + i
+    return sum
+
+# Sum the given numbers
+def sum_numbers(nums):
+    sum = 0
+    for n in nums:
+        sum = sum + n
+    return sum
